1c29e690e0c53eb184e2ba11e7f82900fdcd41ce
javierc22 <javier.ec.22@gmail.com>
2020-07-04 18:08:30 -0400

    Mostrando las categorias en el formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,6 +23,14 @@ const Form = () => {
         <div className="col-md-4">
           <select className="form-control" name="categoria">
             <option value="">-- Selecciona Categoría --</option>
+            {categorias.map(categoria => (
+              <option 
+                key={categoria.strCategory}
+                value={categoria.strCategory}
+              >
+                {categoria.strCategory}
+              </option>
+            ))}
           </select>
         </div>
         <div className="col-md-4">
