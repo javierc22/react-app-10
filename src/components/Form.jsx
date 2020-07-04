cf51a8048efbd4873440ad5b94d2c91a9cb5867b
javierc22 <javier.ec.22@gmail.com>
2020-07-04 18:04:54 -0400

    Consultando la API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,9 +3,7 @@ import { CategoriasContext } from '../context/CategoriasContext';
 
 const Form = () => {
 
-  const { hola } = useContext(CategoriasContext);
-
-  alert(hola);
+  const { categorias } = useContext(CategoriasContext);
 
   return (
     <form className="col-12">
